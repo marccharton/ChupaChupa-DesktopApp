d506ff561f66ed910b4eee075a97dbaca4da94e1
mcharton <marc.charton@epitech.eu>
2014-11-29 10:48:32 +0100

    Derni?res modifs non commit?e : Serialisation du compte, Debut d'article lu, tabitem visibility, Correctifs divers

--- ChupaChupa-DesktopApp/ViewModel/AccountProperties.cs
+++ ChupaChupa-DesktopApp/ViewModel/AccountProperties.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Threading;
 using System.Collections.ObjectModel;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Xml.Serialization;
+using Chupachupa_DesktopApp.Class;
 using ChupaChupa_Model.Entities;
 
 
@@ -17,6 +21,8 @@ namespace Chupachupa_DesktopApp.ViewModel
     /// </summary>
     public partial class ViewModelDataSource : INotifyPropertyChanged
     {
+        private string accountSerializePath = @"account.xml";
+
         private User _currentUser;
         public User CurrentUser
         {
@@ -81,8 +87,13 @@ namespace Chupachupa_DesktopApp.ViewModel
         // ******** Log (in/out) user *********
         private void LogUser()
         {
-            LogUserCmd = new Command(new Action(async () =>
+            LogUserCmd = new CommandWithParameter(new Action<object>(async (o) =>
             {
+                if(o != null)
+                {
+                    var passwordBox = o as PasswordBox;
+                    AccountPasswordText = passwordBox.Password;
+                }
                 if (!IsLoggedIn)
                 {
                     if (AccountLoginText != "" && AccountPasswordText != "")
@@ -94,10 +105,9 @@ namespace Chupachupa_DesktopApp.ViewModel
                         // TODO : Connection à la base avec les données du user
                         await Task.Delay(1000);
                         CurrentUser = new User() { LoginMail = AccountLoginText, Password = AccountPasswordText };
-
-
+                        
                         IsProgressRingActive = false;
-                        SelectedTabIndex = 1;
+                        SelectedTabIndex = 3;
                     }
                 }
                 else
@@ -109,10 +119,69 @@ namespace Chupachupa_DesktopApp.ViewModel
                     SelectedTabIndex = 0;
                 }
             }));
-            
+
+            if (File.Exists(this.accountSerializePath))
+            {
+                if (Unserialize() == true)
+                    LogUserCmd.Execute(null);
+                else
+                {
+                    MessageBox.Show("Ca t'amuses de mettre de la merde dans les credentials ?");
+                }
+            }
         }
 
+        // ******** Serialize Credentials *********
+        private bool Serialize()
+        {
+            List<String> usernameCredantials = new List<string>(){AccountLoginText, AccountPasswordText};
 
+            if (File.Exists(this.accountSerializePath))
+                File.Delete(this.accountSerializePath);
+            Serializer.Serialize(usernameCredantials, this.accountSerializePath, FileMode.OpenOrCreate, typeof(List<string>));
+            return true;
+        }
+
+        // ******** Unserialize Credentials *********
+        private bool Unserialize()
+        {
+            List<String> userCredantials = null;
+            if (File.Exists(this.accountSerializePath))
+                using (var fs = new FileStream(this.accountSerializePath, FileMode.Open))
+                {
+                    try
+                    {
+                        XmlSerializer xml = new XmlSerializer(typeof(List<string>));
+                        userCredantials = xml.Deserialize(fs) as List<string>;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+            if (userCredantials != null)
+            {
+                if (userCredantials.Count != 2)
+                    return false;
+                AccountLoginText = userCredantials[0];
+                AccountPasswordText = userCredantials[1];
+            }
+
+            return true;
+        }
+
+        // ******** Destructor *********
+        ~ViewModelDataSource()
+        {
+            if (IsLoggedIn)
+                Serialize();
+            else
+            {
+                if (File.Exists(this.accountSerializePath))
+                    File.Delete(this.accountSerializePath);
+            }
+        }
+        
 
     }
 
